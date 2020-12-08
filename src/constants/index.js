987271eccbb413e00012fc197ac95889aacb3711
Tima Gixe <timagixe@protonmail.com>
2020-12-08 15:52:02 +0200

    Add month/alphabet and API constnats

---
+++ src/constants/index.js
@@ -0,0 +1,47 @@
+export const alphabetEntries = [
+  ['A', []],
+  ['B', []],
+  ['C', []],
+  ['D', []],
+  ['E', []],
+  ['F', []],
+  ['G', []],
+  ['H', []],
+  ['I', []],
+  ['J', []],
+  ['K', []],
+  ['L', []],
+  ['M', []],
+  ['N', []],
+  ['O', []],
+  ['P', []],
+  ['Q', []],
+  ['R', []],
+  ['S', []],
+  ['T', []],
+  ['U', []],
+  ['V', []],
+  ['W', []],
+  ['X', []],
+  ['Y', []],
+  ['Z', []],
+];
+
+export const monthsEntries = [
+  ['January', []],
+  ['February', []],
+  ['March', []],
+  ['April', []],
+  ['May', []],
+  ['June', []],
+  ['July', []],
+  ['August', []],
+  ['September', []],
+  ['October', []],
+  ['November', []],
+  ['December', []],
+];
+
+export const baseUrl = 'https://yalantis-react-school-api.yalantis.com';
+
+export const pathName = 'api/task0/users';
