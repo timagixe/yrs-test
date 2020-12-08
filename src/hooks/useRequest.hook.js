9c896944b4dc2a2ef7431a6e994a3479bf75f666
Tima Gixe <timagixe@protonmail.com>
2020-12-08 15:51:06 +0200

    Add useRequest custom hook

---
+++ src/hooks/useRequest.hook.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+import { groupEmployeesByAlphabet } from 'utils/groupEmployeesByAlphabet.utility';
+
+export const useRequest = (baseUrl, pathName) => {
+  const [employees, setEmployees] = useState(new Map());
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const { data } = await axios.get(`${baseUrl}/${pathName}`);
+        setEmployees(groupEmployeesByAlphabet(data));
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [baseUrl, pathName]);
+
+  return [employees, setEmployees];
+};
