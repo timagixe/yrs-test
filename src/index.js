2ed2bf95fe08566301abb92810f9a3f777cdc827
Tima Gixe <timagixe@protonmail.com>
2020-12-08 17:03:59 +0200

    Tweak packages and index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from 'components/App/';
 import 'styles/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
